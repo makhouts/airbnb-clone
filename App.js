6347b98d092e744c8d6a88737bc79ec6ccc273d6
Sofiane Makhout <makhouts@gmail.com>
2021-10-27 21:52:58 +0200

    init

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  useColorScheme,
+  View,
+  Text
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <SafeAreaView>
+    </SafeAreaView>
+  );
+};
+
+
+export default App;
---
+++ src/screens/Home/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from 'react-native';
+
+export const HomeScreen = (props) => {
+    return(
+        <View>
+            <Text>comp</Text>
+        </View>
+    );
+};
+
